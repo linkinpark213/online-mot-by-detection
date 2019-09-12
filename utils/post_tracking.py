194ef79c964c443b45438503efdb6ea647b22a54
linkinpark213 <linkinpark213@outlook.com>
2019-09-12 09:31:29 +0800

    Post-tracking cleanup & generating output videos from result files

--- utils/post_tracking.py
+++ utils/post_tracking.py
@@ -1,5 +1,5 @@
+import os
 import argparse
-
 import numpy as np
 
 
@@ -16,33 +16,51 @@ def remove_short_tracks(track_results, min_time_lived):
         else:
             tracks[id] = [line]
 
+    n_deleted_tracks = 0
+    n_deleted_records = 0
     for id, track in tracks.items():
         if track.__len__() < min_time_lived:
-            track[id] = np.array([])
+            n_deleted_tracks += 1
+            n_deleted_records += len(tracks[int(id)])
+            tracks[int(id)] = np.array([])
+    print('{} track(s) ({} records) deleted'.format(n_deleted_tracks, n_deleted_records))
 
     collection = []
     for id, track in tracks.items():
-        collection.append(track)
+        for line in track:
+            collection.append(line)
 
     return np.array(collection)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', type=str, help='Path to the input result file')
-    parser.add_argument('--min_time_lived', type=int, default=10, help='Minimum time lived of tracks')
+    parser.add_argument('input_path', type=str, help='Path to the input result file or directory of input result files')
+    parser.add_argument('--min_time_lived', type=int, default=30, help='Minimum time lived of tracks')
+    parser.add_argument('--output_path', type=str, default='cleaned', help='Path to the output file\'s directory')
     args = parser.parse_args()
 
-    track_results = read_track_file(args.input_file)
-    track_results = remove_short_tracks(track_results, args.min_time_lived)
+    input_files = []
+    if os.path.isfile(args.input_path):
+        input_files = [args.input_path]
+    elif os.path.isdir(args.input_path):
+        input_files = [os.path.join(args.input_path, filename) for filename in os.listdir(args.input_path)]
 
-    filename = args.input_file.split('/')[-1]
-    output_file_name = filename + '_out.txt'
-    output_file = open(output_file_name, 'w+')
+    if not os.path.isdir(args.output_path):
+        os.makedirs(args.output_path)
 
+    for file_path in input_files:
+        print('Processing {}'.format(file_path))
+        input_filename = file_path.split('/')[-1]
+        track_results = read_track_file(file_path)
+        track_results = remove_short_tracks(track_results, args.min_time_lived)
 
-    for line in track_results:
-        output_file.write(
-            '{:d}, {:d}, {:.2f}, {:.2f}, {:.2f}, {:.2f}\n'.format(line[0], line[1], line[2], line[3], line[4], line[5]))
+        output_filename = os.path.join(args.output_path, input_filename)
+        output_file = open(output_filename, 'w+')
 
-    output_file.close()
+        for line in track_results:
+            output_file.write(
+                '{:d}, {:d}, {:.2f}, {:.2f}, {:.2f}, {:.2f}\n'.format(int(line[0]), int(line[1]), line[2], line[3],
+                                                                      line[4], line[5]))
+
+        output_file.close()
--- utils/vis.py
+++ utils/vis.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 _colors = [(47, 47, 211),
            (74, 195, 139),
@@ -50,7 +51,7 @@ def draw_tracklets(image, tracklets, confirmed_only=True):
     :return: A 3D numpy array with shape (h, w, 3). The video frame with boxes of tracklets drawn.
     """
     for tracklet in tracklets:
-        if tracklet.is_confirmed():
+        if (not confirmed_only) or (confirmed_only and tracklet.is_confirmed()):
             box = tracklet.last_box
             image = cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])),
                                   _colors[tracklet.id % _colors.__len__()],
@@ -60,3 +61,30 @@ def draw_tracklets(image, tracklets, confirmed_only=True):
             image = cv2.circle(image, (int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)), radius=10,
                                color=(0, 0, 255), thickness=-1)
     return image
+
+
+def generate_video_from_results_file(video_path, results_path, output_path, show=False):
+    results = np.loadtxt(results_path)
+    capture = cv2.VideoCapture(video_path)
+    writer = None
+    n_frames = 0
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        if writer is None:
+            writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'MPEG'), 30,
+                                     (frame.shape[1], frame.shape[0]))
+        for box in results[np.where(results[:, 0] == n_frames)]:
+            frame = cv2.rectangle(frame, (int(box[2]), int(box[3])), (int(box[2] + box[4]), int(box[3] + box[5])),
+                                  _colors[box[1] % _colors.__len__()], thickness=3)
+            frame = cv2.putText(frame, '{:d}'.format(box[1]), (int(box[0]), int(box[1]) - 8),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, _colors[box[0] % _colors.__len__()], thickness=2)
+            frame = cv2.circle(frame, (int(box[0] + box[2] / 2), int(box[1] + box[3] / 2)), radius=10,
+                               color=(0, 0, 255), thickness=-1)
+        writer.write(frame)
+        if show:
+            cv2.imshow('Output', frame)
+            cv2.waitKey(1)
+    if writer is not None:
+        writer.close()
