90cbf41558b6b74f128b2b97d3d33b219d536d7b
Fangbin Wan <1410638914@qq.com>
2019-09-20 16:41:48 +0800

    add torch.mm distance
    add torch.mm distance

--- mot/metric/__init__.py
+++ mot/metric/__init__.py
@@ -2,4 +2,5 @@ from .metric import Metric
 from .iou import IoUMetric
 from .cosine import CosineMetric
 from .euclidean import EuclideanMetric
+from .mm import MMMetric
 from .combined import CombinedMetric, ProductMetric, SummationMetric
