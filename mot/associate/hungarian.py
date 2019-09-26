8648dac12fdb1c0d92f5339a14df3626db91189f
linkinpark213 <linkinpark213@outlook.com>
2019-09-26 09:09:42 +0800

    Hungarian debugging #3

--- mot/associate/hungarian.py
+++ mot/associate/hungarian.py
@@ -11,13 +11,15 @@ class HungarianMatcher(Matcher):
     def __call__(self, tracklets, detection_boxes, img):
         similarity_matrix, features = self.metric(tracklets, detection_boxes, img)
         row_ind, col_ind = scipy.optimize.linear_sum_assignment(1 - similarity_matrix)
-        indices_to_remove = []
+        rows_to_remove = []
+        cols_to_remove = []
         for i in range(len(row_ind)):
             if similarity_matrix[row_ind[i], col_ind[i]] < self.sigma:
-                indices_to_remove.append(i)
+                rows_to_remove.append(row_ind[i])
+                cols_to_remove.append(col_ind[i])
         row_ind = row_ind.tolist()
         col_ind = col_ind.tolist()
-        for index in indices_to_remove:
-            row_ind.remove(row_ind[index])
-            col_ind.remove(col_ind[index])
+        for i in range(len(rows_to_remove)):
+            row_ind.remove(rows_to_remove[i])
+            col_ind.remove(cols_to_remove[i])
         return np.array(row_ind), np.array(col_ind), features
