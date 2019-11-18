a9097008addd0cdb5b0ab3928dbe1d8ad8fee63d
linkinpark213 <linkinpark213@outlook.com>
2019-11-18 11:43:23 +0800

    Removed MMMetric (similar to cosine?)

--- mot/metric/__init__.py
+++ mot/metric/__init__.py
@@ -2,5 +2,4 @@ from .metric import Metric
 from .iou import IoUMetric
 from .cosine import CosineMetric
 from .euclidean import EuclideanMetric
-from .mm import MMMetric
 from .combined import CombinedMetric, ProductMetric, SummationMetric
