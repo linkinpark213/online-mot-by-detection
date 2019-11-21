1de685a563f303c7ea9f740e0d5dc0612948dc15
linkinpark213 <linkinpark213@outlook.com>
2019-11-21 10:39:22 +0800

    Added basic Tracktor implementation

---
+++ examples/tracktor.py
@@ -0,0 +1,94 @@
+import mot.encode
+import mot.metric
+import mot.detect
+import mot.predict
+import numpy as np
+import mot.associate
+import mot.utils.box
+from mot.tracker import Tracker, Tracklet
+
+
+class CustomTracker(Tracker):
+    def __init__(self, sigma_active=0.5, lambda_active=0.6, lambda_new=0.3):
+        detector = mot.detect.Detectron(
+            'https://raw.githubusercontent.com/facebookresearch/detectron2/22e04d1432363be727797a081e3e9d48981f5189/configs/COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml',
+            'detectron2://COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x/139686956/model_final_5ad38f.pkl')
+        iou_metric = mot.metric.IoUMetric(use_prediction=True)
+        iou_matcher = mot.associate.HungarianMatcher(iou_metric, sigma=0.5)
+
+        matcher = iou_matcher
+        predictor = mot.predict.DetectronRCNNPredictor(
+            'https://raw.githubusercontent.com/facebookresearch/detectron2/22e04d1432363be727797a081e3e9d48981f5189/configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml',
+            'detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl'
+        )
+        # self.predictor = None
+        self.sigma_active = sigma_active
+        self.lambda_active = lambda_active
+        self.lambda_new = lambda_new
+        super().__init__(detector, [], matcher, predictor)
+
+    def update(self, row_ind, col_ind, detections, detection_features):
+        """
+        Update the tracklets.
+        :param row_ind: A list of integers. Indices of the matched tracklets.
+        :param col_ind: A list of integers. Indices of the matched detections.
+        :param detection_boxes: A list of Detection objects.
+        :param detection_features: The features of the detections. It can be any form you want.
+        """
+        # Update tracked tracklets' features
+        for i in range(len(row_ind)):
+            tracklet = self.tracklets_active[row_ind[i]]
+            tracklet.update(self.frame_num, tracklet.prediction, {'box': tracklet.prediction.box})
+
+        # Deal with unmatched tracklets
+        tracklets_to_kill = []
+        for i, tracklet in enumerate(self.tracklets_active):
+            if tracklet.prediction.score < self.sigma_active:
+                if tracklet.fade():
+                    tracklets_to_kill.append(tracklet)
+        for tracklet in tracklets_to_kill:
+            self.tracklets_active.remove(tracklet)
+            self.tracklets_finished.append(tracklet)
+
+        # Kill tracklets with lower scores using NMS
+        tracklets_to_kill = []
+        for i, tracklet in enumerate(self.tracklets_active):
+            ious = mot.utils.box.iou(tracklet.prediction.box, [t.prediction.box for t in self.tracklets_active])
+            overlapping_boxes = np.argwhere(ious > self.lambda_active)[0]
+            for j in overlapping_boxes:
+                if i == j:
+                    continue
+                else:
+                    if tracklet.prediction.score >= self.tracklets_active[j].prediction.score:
+                        tracklets_to_kill.append(self.tracklets_active[j])
+                    else:
+                        tracklets_to_kill.append(tracklet)
+                        break
+        for tracklet in tracklets_to_kill:
+            self.tracklets_active.remove(tracklet)
+            self.tracklets_finished.append(tracklet)
+
+        # Kill tracklets that failed to regress new box
+
+        # Update tracklets
+        for tracklet in self.tracklets_active:
+            tracklet.last_detection.box = tracklet.prediction.box
+
+        # Remove matched detections
+        detections_to_remove = []
+        for i, detection in enumerate(detections):
+            if i not in col_ind:
+                for tracklet in self.tracklets_active:
+                    if mot.utils.box.iou(detection.box, tracklet.last_detection.box) > self.lambda_new:
+                        detections_to_remove.append(detection)
+                        break
+            else:
+                detections_to_remove.append(detection)
+        for detection in detections_to_remove:
+            detections.remove(detection)
+
+        # Initiate new tracklets
+        for i, detection in enumerate(detections):
+            new_tracklet = Tracklet(0, self.frame_num, detections[i], detection_features[i], max_ttl=1)
+            self.add_tracklet(new_tracklet)
+            self.predictor.initiate([new_tracklet])
--- mot/predict/predict.py
+++ mot/predict/predict.py
@@ -2,24 +2,30 @@ class Predictor:
     def __init__(self):
         raise NotImplementedError('Extend the Predictor class to implement your own prediction method.')
 
-    def __call__(self, tracklet):
-        return self.predict(tracklet)
+    def __call__(self, tracklets, img):
+        return self.predict(tracklets, img)
 
-    def initiate(self, tracklet):
+    def initiate(self, tracklets):
         """
-        Initiate a tracklet with its initial mean and covariance.
+        Initiate tracklets' states that are used by the predictor.
         """
         raise NotImplementedError('Extend the Predictor class to implement your own prediction method.')
 
-    def update(self, tracklet):
+    def update(self, tracklets):
         """
-        Update a tracklet with its last box coordinates.
+        Update tracklets' states that are used by the predictor.
         """
         raise NotImplementedError('Extend the Predictor class to implement your own prediction method.')
 
-    def predict(self, tracklet):
+    def predict(self, tracklets, img):
         """
         Predict state in the following time step.
-        :return: Predicted state in the following time step.
+        :return: A list of Prediction objects corresponding to the tracklets.
         """
         raise NotImplementedError('Extend the Predictor class to implement your own prediction method.')
+
+
+class Prediction:
+    def __init__(self, box, score):
+        self.box = box
+        self.score = score
