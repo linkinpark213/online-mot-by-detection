81a08ffa3f3641039ea5996419f4bba0873dae29
linkinpark213 <linkinpark213@outlook.com>
2019-11-28 20:35:28 +0800

    Evaluation bug debugging 6

--- mot/detect/mot_public_detector.py
+++ mot/detect/mot_public_detector.py
@@ -10,6 +10,7 @@ class MOTPublicDetector(Detector):
         self.detections = self.detections[:, [0, 2, 3, 4, 5, 6]]
         self.detections[:, 3] = self.detections[:, 3] + self.detections[:, 1]
         self.detections[:, 4] = self.detections[:, 4] + self.detections[:, 2]
+        self.detections = self.detections.astype(np.float32)
         self.detections = self.detections[np.where(self.detections[:, 5] > conf_threshold)]
 
     def __call__(self, img):
