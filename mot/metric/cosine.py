fb3da54bcbf191a7aa172dae3fbc5a2cf89ee5d5
linkinpark213 <linkinpark213@outlook.com>
2019-11-28 23:04:45 +0800

    Add history tracing-back to cosine metric

--- mot/metric/cosine.py
+++ mot/metric/cosine.py
@@ -9,16 +9,20 @@ class CosineMetric(Metric):
     An affinity metric that considers the cosine of angles between tracklets' features and detections' features.
     """
 
-    def __init__(self, encoding):
+    def __init__(self, encoding, history=1):
         super(CosineMetric).__init__()
         self.encoding = encoding
+        self.history = history
 
     def __call__(self, tracklets, detection_features):
         matrix = np.zeros([len(tracklets), len(detection_features)])
         for i in range(len(tracklets)):
             for j in range(len(detection_features)):
-                matrix[i][j] = self.cos(tracklets[i].feature_history[-1][1][self.encoding],
-                                        detection_features[j][self.encoding])
+                affinities = []
+                for k in range(max(self.history, len(tracklets.feature_history))):
+                    affinities.append(self.cos(tracklets[i].feature_history[-k - 1][1][self.encoding],
+                                               detection_features[j][self.encoding]))
+                matrix[i][j] = sum(affinities) / len(affinities)
         mot.utils.debug.log_affinity_matrix(matrix, tracklets, self.encoding)
         return matrix
 
