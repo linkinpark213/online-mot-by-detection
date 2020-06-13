5b7ac5f7c8d02f293cc676717691c349b3a7df01
linkinpark213 <linkinpark213@outlook.com>
2020-06-13 20:32:34 +0900

    1. Removed currently inactive detectors

--- mot/detect/__init__.py
+++ mot/detect/__init__.py
@@ -4,10 +4,7 @@ from .detect import Detector, DETECTOR_REGISTRY, build_detector
 
 if imputil.find_spec('detectron2'):
     from .detectron2 import Detectron
-    if imputil.find_spec('centermask'):
-        from .centermask2 import CenterMaskDetector
 if imputil.find_spec('mmdet'):
     from .mmdetection import MMDetector
 from .centernet import CenterNetDetector
-from .centertrack import CenterTrackDetector
 from .mot_public_detector import MOTPublicDetector
